66763ce075b8e13561f89295a770f1aef0fa25c9
santi <santiago.penate.vera@gmail.com>
2025-01-12 11:26:47 +0000

    Improved comments on the scripts

--- session1_10_01_2025/con_analysis.py
+++ session1_10_01_2025/con_analysis.py
@@ -6,29 +6,46 @@ from GridCalEngine.Simulations.PowerFlow.power_flow_worker import multi_island_p
 # load a grid (.gridcal, .m (Matpower), .raw (PSS/e) .rawx (PSS/e), .epc (PowerWorld), .dgs (PowerFactory)
 grid = gce.open_file("case89pegase.m")
 
+# we compile the grid into a numerical circuit for a particular time point (in this case, the snapshot)
 nc = gce.compile_numerical_circuit_at(circuit=grid, t_idx=None)
 
+# define the power flow options
 opt = gce.PowerFlowOptions(solver_type=gce.SolverType.DC)
+
+# run a powerr flow over the numerical circuit: This is a custom operation,
+# since we want more fine control over the process
 res_base = multi_island_pf_nc(nc, options=opt)
 
+# gather the number of branches (lines, dc-lines, transformers, windings, series, impedances, switches)
 m = nc.passive_branch_data.nelm
+
+# declare the result matrices
 con_flows = np.zeros((m, m))
 lodf = np.zeros((m, m))
 
+# loop over the number of branches
 for k in range(m):
 
+    # collect the state of a branch
     prev_state = nc.passive_branch_data.active[k]
 
-    # failing the branch
+    # failing the branch; Note we're directly modifying the numerical circuit instead of the MultiCircuit
+    # this renders a much faster execution, since we're skipping the compilation step
     nc.passive_branch_data.active[k] = 0
 
+    # Run the power flow on the modified numerical circuit
     res = multi_island_pf_nc(nc, options=opt)
+
+    # Record the flows in MW
     con_flows[k, :] = res.Sf.real
+
+    # Compute the Line-outage-distribution-factors (LODF)
     lodf[k, :] = (res_base.Sf.real - res.Sf.real) / (res_base.Sf.real + 1e-20)
 
-    # recovering the branch
+    # recover the branch to the previous state
     nc.passive_branch_data.active[k] = prev_state
 
+# Save the flows to CSV using pandas
 df1 = pd.DataFrame(
     data=con_flows,
     index=nc.passive_branch_data.names,
@@ -36,6 +53,7 @@ df1 = pd.DataFrame(
 )
 df1.to_csv("Contingencies.csv")
 
+# Save the LODF to csv using pandas
 df2 = pd.DataFrame(
     data=lodf,
     index=nc.passive_branch_data.names,
--- session1_10_01_2025/opf_comparison.py
+++ session1_10_01_2025/opf_comparison.py
@@ -4,18 +4,19 @@ import GridCalEngine as gce
 # load a grid (.gridcal, .m (Matpower), .raw (PSS/e) .rawx (PSS/e), .epc (PowerWorld), .dgs (PowerFactory)
 grid = gce.open_file("case89pegase.m")
 
-# declare the snapshot opf
+# Run the OPF in linear mode: This will use a linear solver library (Highs by default)
 opf_options = gce.OptimalPowerFlowOptions(mip_solver=gce.MIPSolvers.HIGHS)
 opf_driver = gce.OptimalPowerFlowDriver(grid=grid, options=opf_options)
 opf_driver.run()
 lin_opf_res = opf_driver.results
 
-
-opf_options = gce.OptimalPowerFlowOptions(solver = gce.SolverType.NONLINEAR_OPF)
+# Run the OPF in non-linear mode: This will use the interior-point solver from Gridcal
+opf_options = gce.OptimalPowerFlowOptions(solver=gce.SolverType.NONLINEAR_OPF)
 nlopf_driver = gce.OptimalPowerFlowDriver(grid=grid, options=opf_options)
 nlopf_driver.run()
 nl_opf_res = nlopf_driver.results
 
+# Plot the comparison
 fig = plt.figure()
 plt.ion()
 plt.plot(lin_opf_res.Sf.real, label="Linear Pf")
